bdb167c5534c2a02ef0c7ee592e097494392ca02
BartekWas <bartek1wasik@gmail.com>
2019-04-16 20:54:06 +0200

    register activity added

--- app/src/main/java/com/example/kejapp/view/LoginActivity.java
+++ app/src/main/java/com/example/kejapp/view/LoginActivity.java
@@ -2,9 +2,14 @@ package com.example.kejapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.kejapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +17,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final TextView registerText = findViewById(R.id.registerText);
+        final Button login = findViewById(R.id.loginButton);
+
+        registerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                intent.putExtra("object 1", "dupa");
+                startActivity(intent);
+            }
+        });
+
     }
 }
