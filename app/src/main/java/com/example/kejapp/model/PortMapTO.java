7fcaf04dc79df93b269762cbee7f46909b8c463f
rozekm <mateusz.s.rozek@gmail.com>
2019-05-08 20:22:29 +0200

    PortMapTO update

--- app/src/main/java/com/example/kejapp/model/PortMapTO.java
+++ app/src/main/java/com/example/kejapp/model/PortMapTO.java
@@ -23,6 +23,38 @@ public class PortMapTO implements Serializable {
     @Expose
     private Double longitude;
 
+    @SerializedName("freeQuaysQuantity")
+    @Expose
+    int freeQuaysQuantity;
+
+    @SerializedName("allQuaysQuantity")
+    @Expose
+    int allQuaysQuantity;
+
+    @SerializedName("calculatedPrice")
+    @Expose
+    Double calculatedPrice;
+
+    @SerializedName("engineForbidden")
+    @Expose
+    Boolean engineForbidden;
+
+    @SerializedName("gasStation")
+    @Expose
+    Boolean gasStation;
+
+    @SerializedName("medicalCare")
+    @Expose
+    Boolean medicalCare;
+
+    @SerializedName("sailingStore")
+    @Expose
+    Boolean sailingStore;
+
+    @SerializedName("nightLights")
+    @Expose
+    Boolean nightLights;
+
     public Long getId() {
         return id;
     }
@@ -53,5 +85,69 @@ public class PortMapTO implements Serializable {
 
     public void setLongitude(Double longitude) {
         this.longitude = longitude;
+    }
+
+    public int getFreeQuaysQuantity() {
+        return freeQuaysQuantity;
+    }
+
+    public void setFreeQuaysQuantity(int freeQuaysQuantity) {
+        this.freeQuaysQuantity = freeQuaysQuantity;
+    }
+
+    public int getAllQuaysQuantity() {
+        return allQuaysQuantity;
+    }
+
+    public void setAllQuaysQuantity(int allQuaysQuantity) {
+        this.allQuaysQuantity = allQuaysQuantity;
+    }
+
+    public Double getCalculatedPrice() {
+        return calculatedPrice;
+    }
+
+    public void setCalculatedPrice(Double calculatedPrice) {
+        this.calculatedPrice = calculatedPrice;
+    }
+
+    public Boolean getEngineForbidden() {
+        return engineForbidden;
+    }
+
+    public void setEngineForbidden(Boolean engineForbidden) {
+        this.engineForbidden = engineForbidden;
+    }
+
+    public Boolean getGasStation() {
+        return gasStation;
+    }
+
+    public void setGasStation(Boolean gasStation) {
+        this.gasStation = gasStation;
+    }
+
+    public Boolean getMedicalCare() {
+        return medicalCare;
+    }
+
+    public void setMedicalCare(Boolean medicalCare) {
+        this.medicalCare = medicalCare;
+    }
+
+    public Boolean getSailingStore() {
+        return sailingStore;
+    }
+
+    public void setSailingStore(Boolean sailingStore) {
+        this.sailingStore = sailingStore;
+    }
+
+    public Boolean getNightLights() {
+        return nightLights;
+    }
+
+    public void setNightLights(Boolean nightLights) {
+        this.nightLights = nightLights;
     }
 }
