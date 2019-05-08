67ad12a6a5e60a95aac34a29660273fc925d515f
rozekm <mateusz.s.rozek@gmail.com>
2019-05-08 20:25:44 +0200

    ReservationTO update

--- app/src/main/java/com/example/kejapp/model/ReservationTO.java
+++ app/src/main/java/com/example/kejapp/model/ReservationTO.java
@@ -4,19 +4,44 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ReservationTO implements Serializable {
 
-    @SerializedName("pierId")
+    @SerializedName("quayInfoTO")
     @Expose
-    String pierId;
+    QuayInfoTO quayInfoTO;
 
-    @SerializedName("freeQuaysNumbers")
+    @SerializedName("startDate")
     @Expose
-    List<Long> freeQuaysNumbers;
+    LocalDateTime startDate;
 
-    @SerializedName("allQuaysQuantity")
+    @SerializedName("endDate")
     @Expose
-    int allQuaysQuantity;
+    LocalDateTime endDate;
+
+    public QuayInfoTO getQuayInfoTO() {
+        return quayInfoTO;
+    }
+
+    public void setQuayInfoTO(QuayInfoTO quayInfoTO) {
+        this.quayInfoTO = quayInfoTO;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
 }
